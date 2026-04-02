76b48c216ca9e3e10403cbca57fe9b68951c37cf
Naif-Alnahdi-281 <445050281@std.psau.edu.sa>
2026-04-02 16:53:42 +0300

    Add priority parameter

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -256,7 +256,8 @@ public class SchedulerSimulation {
             Process process = new Process("P" + i, burstTime, timeQuantum, priority);
             
             // Create a new process object with a unique name, burst time, and the defined time quantum
-            Process process = new Process("P" + i, burstTime, timeQuantum);
+            // FEATURE 1: Added priority parameter
+            Process process = new Process("P" + i, burstTime, timeQuantum, priority);
             
             // Add the process to the ready queue and the map
             addProcessToQueue(process, processQueue, processMap);
